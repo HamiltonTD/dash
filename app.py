5ab991e15b0e764afe20fc167123daa2c8a3dcad
HamiltonTD <thami583@mtroyal.ca>
2021-03-23 13:10:08 -0600

    favicon & css styling added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import dash
-import dase_core_components as dcc
+import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 
@@ -8,37 +8,74 @@ data = data.query("type == 'conventional' and region== 'Albany'")
 data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
 data.sort_values("Date", inplace=True)
 
-app = dash.Dash(__name__)
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+        "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Avocado Dashboard"
 
 app.layout = html.Div(
     children=[
-        html.H1(children="Avocado Analytics",),
-        html.P(
-            children="Analyzing avocado prices",
+        html.Div(
+            children=[
+                html.P(
+                    children="🥑",
+                    className="header-emoji"
+                       ),
+                html.H1(
+                    children="Avocado Analytics",
+                    className="header-title",
+                ),
+                html.P(
+                    children="Analyzing avocado prices",
+                    className="header-description"
+                ),
+            ],
+            className="header"
         ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Date"],
-                        "y": data["AveragePrice"],
-                        "type": "lines",
-                    },
-                ],
-                "layout=" {"title": "Average Price of Avocados"},
-            },
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="price-chart",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                                {
+                                    "x": data["Date"],
+                                    "y": data["AveragePrice"],
+                                    "type": "lines",
+                                    "hovertemplate": "$%{y:.2f}"
+                                    "<extra></extra>",
+                                },
+                            ],
+                            "layout": {
+                                "title": {
+                                    "text": "Average Price of Avocados",
+                                    "x": 0.05,
+                                    "xanchor": "left",
+                                },
+                                "xaxis": {"fixedrange": True},
+                                "yaxis": {
+                                    "tickprefix": "$",
+                                    "fixedrange": True,
+                                },
+                                "colorway": ["#17B897"],
+                            },
+                        },
+                    ),
+                    className="card",
+                ),
+            ],
         ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Date"],
-                        "y": data["Total Volume"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Avocados Sold"},
-            },
-        ),
-    ]
+    ],
 )
+                
+                
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
